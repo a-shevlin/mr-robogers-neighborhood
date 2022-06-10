87f83b562bc66c5cc59419d838d28f20e396a875
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-10 09:53:42 -0700

    create form to take user input and display stringOut back to user.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function inputArray(input) {
   const array = [];
-  for (i = 0; i <= input; i++) {
+  for (let i = 0; i <= input; i++) {
     array.push(i);
   }
   const stringArray = array.toString().split(",");
@@ -23,3 +23,18 @@ function inputRobo(input) {
   });
   return roboArray;
 }
+
+$(document).ready(function() {
+  $("form#input").submit(function(event) {
+    event.preventDefault();
+    const input = $("input#number").val();
+    const output = inputRobo(input);
+    let stringOut = output.forEach(function(element){
+      output.push(number + " ");
+    });
+
+    // $("#input").hide();
+    // $("#result").show(1000);
+    // $("#user1").append(input);
+  });
+})
